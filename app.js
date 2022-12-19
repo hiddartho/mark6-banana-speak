bdf75448c223dd86190bc8a520dc08656666ad5c
hiddartho <56486358+hiddartho@users.noreply.github.com>
2022-12-19 09:43:03 +0530

    taking input

---
+++ app.js
@@ -0,0 +1,11 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+
+
+
+
+function clickHandler(){
+    console.log("clicked")
+    console.log(txtInput.value)
+}
+btnTranslate.addEventListener("click", clickHandler)
