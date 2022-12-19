29d27b0116a3e7f8bb86237c928d71b18771cae3
hiddartho <56486358+hiddartho@users.noreply.github.com>
2022-12-19 11:59:09 +0530

    chore: updated tasks

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
 
+var serverURL= "https://api.funtranslations.com/translate/minion.json"
 
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+ text
+}
 
+function errorHandler(){
+    console.log("error occured", error)
+}
 
 function clickHandler(){
-    console.log("clicked")
-    console.log(txtInput.value)
+    var inputText= txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response=>response.json())
+        .then(json=>{
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 }
+
 btnTranslate.addEventListener("click", clickHandler)
